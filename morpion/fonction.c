4fc534167678d81271b05fb5fcfbe1de74518328
jkotkpo23 <kotokpj@gmail.com>
2024-09-17 22:54:05 +0200

    Version jouable

--- morpion/fonction.c
+++ morpion/fonction.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "fonction.h"
 
+
+
 int * allocOneDim(int n){
     int * tab;
     tab = (int *)malloc(n * sizeof(int));
@@ -55,10 +57,94 @@ void printBoard(gameBoard board){
     }
 }
 
+int canPlay(gameBoard board, int i, int j){
+    return board[i][j] == 0;
+}
+
+gameBoard play(gameBoard board, int turn, int i, int j){
+    
+    board[i][j] = turn;
+    return board;
+}
+
+void changeTurn(int *turn){
+    if(*turn == 1) *turn = -1;
+    else *turn = 1;
+}
+
+int checkLine(gameBoard board, int turn, int i){
+    int count = 0;
+    while(count < 3 && board[i][count] == turn) count++;
+    return(count == 3);
+}
+
+int checkColomn(gameBoard board, int turn, int j){
+    int count = 0;
+    while(count < 3 && board[count][j] == turn) count++;
+    return(count == 3);
+}
+
+int checkFirstDiag(gameBoard board, int turn, int i, int j){
+    int count = 0;
+    if (i == j) 
+        while(count < 3 && board[count][count] == turn) count++;
+    return(count == 3);
+}
+
+int checkSecDiag(gameBoard board, int turn, int i, int j){
+    int count = 0;
+    if(i + j == 2)
+        while(count < 3 && board[count][2 - count] == turn) count++;
+    return count == 3;
+    
+}
+
+int checkingWin(gameBoard board, int turn, int i, int j) {
+    return(checkLine(board, turn, i) || 
+        checkColomn(board, turn, j) || 
+        checkFirstDiag(board, turn, i, j) || 
+        checkSecDiag(board, turn, i, j));
+}
+
+int checkinDraw(gameBoard board){
+    int count = 0;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] != 0) count++;
+        }
+    }
+    return count == 9;
+}
+
+int isGoingGame(gameBoard board){
+    return (!checkinDraw(board));
+}
+
+
+void playing(gameBoard board){
+    int turn = 1;
+    int i, j;
+    int winning = 0;
+    while (isGoingGame(board) && !winning){
+        printf("Joueur %d, entrez i\n", turn); scanf("%d", &i);
+        printf("Joueur %d, entrez j\n", turn); scanf("%d", &j);
+        if(canPlay(board, i, j)){
+            board = play(board, turn, i, j);
+            printBoard(board);
+            winning = checkingWin(board, turn, i, j);
+            if(winning) printf("Game won\n");
+            
+            changeTurn(&turn);
+        }
+    }
+    
+}
+
 int main(){
     
     gameBoard board = init_board();
     printBoard(board);
+    playing(board);
     board = cleanBoard(board);
     
     return 0;
--- morpion/fonction.h
+++ morpion/fonction.h
@@ -5,6 +5,24 @@
 
 typedef int ** gameBoard;
 
+typedef struct {
+    char letter;
+    int row;
+    int col;
+} coordinate;
+
+coordinate dictionary[] = {
+    {'a', 0, 0},
+    {'z', 0, 1},
+    {'e', 0, 2},
+    {'q', 1, 0},
+    {'s', 1, 1},
+    {'d', 1, 2},
+    {'w', 2, 0},
+    {'x', 2, 1},
+    {'c', 2, 2}
+};
+
 
 gameBoard init_board();
 int** cleanBoard(int ** mat);
